c7c46a24bfb6fb7b218de2dbd7184915ffe94dde
Alvaro <Alvaro@MacBook-Air-de-Alvaro-5.local>
2018-11-29 10:36:41 +0100

    commit en fix

--- seqClass.sh
+++ seqClass.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 seq=$1
+seq=$(echo $seq | tr a-z A-Z) 
 if [[ $seq =~ ^[ACGTU]+$ ]]; then
   if [[ $seq =~ T ]]; then
     echo "The sequence is DNA"
@@ -10,4 +11,13 @@ if [[ $seq =~ ^[ACGTU]+$ ]]; then
   fi
 else
   echo "The sequence is not DNA nor RNA"
+fi
+motif=$(echo $2 | tr a-z A-Z)
+if [[ -n $motif ]]; then
+  echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
+  if [[ $seq =~ $motif ]]; then
+    echo "FOUND"
+  else
+    echo "NOT FOUND"
+  fi
 fi
