36aac5dbf9e699844021dcd2734e5e33526ce459
Alvaro <Alvaro@MacBook-Air-de-Alvaro-5.local>
2018-11-29 10:57:57 +0100

    Cambio en fix

--- seqClass.sh
+++ seqClass.sh
@@ -16,8 +16,8 @@ motif=$(echo $2 | tr a-z A-Z)
 if [[ -n $motif ]]; then
   echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
   if [[ $seq =~ $motif ]]; then
-    echo "FOUND"
+    echo "PATATA"
   else
-    echo "NOT FOUND"
+    echo "NO PATATA"
   fi
 fi
